35b13483254940de0d5f2ff122cb7bbb1033ddaf
Raghav Goyal <raghavgoyal492.2805@gmail.com>
2022-06-15 21:05:41 +0530

    add client

---
+++ client/src/utils/constant.js
@@ -0,0 +1,8 @@
+import stackingAbi from "./stacking.json";
+import rewardTokenAbi from "./rewardToken.json";
+
+const stackAbi = stackingAbi;
+const stackAddress = "0x43bC4A984166D223062e6B58541206dfe71Fc1eE";
+
+const rewardTokenAbi = rewardTokenAbi;
+const rewardTokenAddress = "0x8671D2D717A3443356FdDaB27D8AAcEa0d355fD8";
